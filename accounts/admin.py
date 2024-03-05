86c7896c80d5dd798c6fb6ce3dfbe49b30df51ce
Frederico27 <fredericomarcal27@gmail.com>
2024-03-05 22:09:42 +0900

    second push

--- accounts/admin.py
+++ accounts/admin.py
@@ -4,12 +4,45 @@ from django.contrib import admin
 
 #import some models
 # from .models import Customer
+from .models import Product, Tag
 
 #import all models
 from .models import *
 
-admin.site.register(Customer)
-admin.site.register(Product)
-admin.site.register(Order)
+
+#display title of column in admin template table
+class accountsAdminCustomer(admin.ModelAdmin):
+    list_display = ('name', 'phone', 'email', 'date_created')
+    list_filter = ('name', 'phone', 'email', 'date_created')
+    search_fields = ('name', 'phone', 'email', 'date_created')
+    
+class accountsAdminProduct(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category', 'description', 'Tag',  'date_created')
+    list_filter = ('name', 'price', 'category', 'description',  'date_created')
+    search_fields = ('name', 'price', 'category', 'description', 'date_created')
+   
+   #How to display many to many column to admin template table
+    def Tag(self, request):
+        product = Product.objects.get(id=request.id)
+        tags = product.tags.all()
+        
+        tag_values = [tag.name for tag in tags] 
+        return tag_values
+
+class accountsAdminOrder(admin.ModelAdmin):
+    list_display = ('customer', 'product', 'date_created','status')
+    list_filter = ('customer', 'product', 'date_created','status')
+    search_fields = ('customer', 'product','date_created','status')
+    
+class accountsAdminTags(admin.ModelAdmin):
+    search_fields = ('name')
+    list_display = ('name')
+    list_filter = ('name')
+    
+
+admin.site.register(Customer, accountsAdminCustomer)
+admin.site.register(Product, accountsAdminProduct)
+admin.site.register(Order, accountsAdminOrder)
+admin.site.register(Tag)
 
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import * #import all (Product, Customer, Order, Tag)
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customer = Customer.objects.all()
+    
+    total_customer = customer.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    
+    
+    context = {'orders': orders, 'customers': customer,
+    'total_orders':total_orders, 'total_customer':total_customer, 
+    'delivered':delivered, 'pending':pending}
+    
+    return render(request, 'accounts/dashboard.html', context)
 
 def product(request):
-    return render(request, 'accounts/product.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/product.html', {'products': products})
 
 def customer(request):
     return render(request, 'accounts/customer.html')
